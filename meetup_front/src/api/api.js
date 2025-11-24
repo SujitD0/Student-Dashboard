73df3802087196f2a6ae0cc8b2d006fbef9b139e
vadervon <sujitdedwal2000@gmail.com>
2025-11-24 17:02:45 +0530

    added environment variables

--- meetup_front/src/api/api.js
+++ meetup_front/src/api/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://student-dashboard-kh1m.onrender.com/api/",
+  baseURL: import.meta.env.VITE_API_URL || "https://student-dashboard-kh1m.onrender.com/api/",
 });
 
 
