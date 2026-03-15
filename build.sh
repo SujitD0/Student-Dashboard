5a00f9b47be79e386fa220bf9cc592a0a01f925b
Alternate <alternates.app@gmail.com>
2026-03-15 15:38:38 +0530

    fix: add makemigrations to build.sh to create pending migrations on deploy

--- build.sh
+++ build.sh
@@ -9,6 +9,9 @@ pip install -r requirements.txt
 # Collect static files
 python manage.py collectstatic --no-input
 
+# Create any pending migrations
+python manage.py makemigrations
+
 # Run migrations
 python manage.py migrate
 
